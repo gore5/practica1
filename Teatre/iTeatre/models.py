656945108b330cd246c21bbc1e4059500ec2a991
Carolina Romeu Farre <crf1@pl-cap013.udl.net>
2013-04-08 13:45:31 +0200

    bd

--- Teatre/iTeatre/models.py
+++ Teatre/iTeatre/models.py
@@ -6,37 +6,60 @@ from django.contrib.auth.models import User
 
 class Escriptor(models.Model):
 	nom = models.CharField(max_length=40)
+	edat = models.IntegerField()
 
 	def __unicode__(self):
 		return self.nom
+
 
 class Actor(models.Model):
 	nom = models.CharField(max_length=40)
 	edat = models.IntegerField()
 	sexe = models.CharField(max_length=1)
 	
+	
 	def __unicode__(self):
 	        return self.nom
 
 
-
 class Director(models.Model):
 	nom = models.CharField(max_length=40)
+	edat = models.IntegerField()
+	sexe = models.CharField(max_length=1)
 	
 
 	def __unicode__(self):
 		return self.nom
 
-class Obra_Teatre(models.Model):
+
+class nom_Obra (models.Model):
 	nomObra = models.CharField(max_length=40)
-	Tipus = models.CharField(max_length=40)
-	date = models.DateTimeField()
-	actors = models.ManyToManyField(Actor)
-	escriptor = models.ForeignKey(Escriptor)
-	director = models.ForeignKey(Director)
 
 	def __unicode__(self):
 		return self.nomObra
+	
+
+class Representacio (models.Model):
+	nomObra = models.ForeignKey(nom_Obra)
+	dataInici = models.DateTimeField()
+	dataFi = models.DateTimeField()
+	actors = models.ManyToManyField(Actor)
+	director = models.ForeignKey(Director)
+	
+
+	def __unicode__(self):
+		return self.nomObra
+
+
+class Obra_Teatre(models.Model):
+	nomObra = models.ForeignKey(nom_Obra)
+	Tipus = models.CharField(max_length=40)
+	escriptor = models.ForeignKey(Escriptor)
+	representacions = models.ManyToManyField(Representacio)
+
+	def __unicode__(self):
+		return self.nomObra
+
 
 
 
